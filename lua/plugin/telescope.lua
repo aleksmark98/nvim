848ebf98da38d7624dda75fa3b59cf0f085d5a22
aleks <aleksmark98@gmail.com>
2025-06-08 17:43:44 +0200

    telescope: add function for find_files and then grep

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -3,14 +3,15 @@ return {
       'nvim-telescope/telescope.nvim',
       keys = {
          {'<leader>ff',  '<cmd>Telescope find_files<CR>'}, -- uses FZF via an extention
+         {'<leader>gr',  '<cmd>lua FZF_THEN_GREP()<CR>'}, -- native - uses ripgrep without args;
          {'<leader>rg',  '<cmd>Telescope live_grep<CR>'}, -- native - uses ripgrep without args;
          {'<leader>rh',  "<cmd>lua require'telescope.builtin'.live_grep({ layout_strategy='horizontal' })<CR>"},
          {'<leader>nvc',  "<cmd>lua require'telescope.builtin'.live_grep({ cwd='~/.config/nvim/' })<CR>"},
          {'<leader>fb',  '<cmd>Telescope buffers<CR>'}, -- can be done natively with ":ls<cr>:b<space>"
          {'<leader>fh',  '<cmd>Telescope help_tags<CR>'},
-         {'<leader>gd',  '<cmd>Telescope lsp_definitions<CR>'},
-         {'<leader>gr',  '<cmd>Telescope lsp_references<CR>'},
-         {'<leader>ld',  '<cmd>Telescope diagnostics<CR>'},
+         {'<leader>ld',  '<cmd>Telescope lsp_definitions<CR>'},
+         {'<leader>lr',  '<cmd>Telescope lsp_references<CR>'},
+         {'<leader>fd',  '<cmd>Telescope diagnostics<CR>'},
          {'<leader>gf',  '<cmd>Telescope git_files<CR>'},
          {'<leader>gc',  '<cmd>Telescope git_commits<CR>'},
          {'<leader>gbc', '<cmd>Telescope git_bcommits<CR>'},
@@ -57,6 +58,35 @@ return {
          }
          telescope.load_extension('fzf')
          telescope.load_extension('rooter')
-      end
+      end,
+      init = function()
+         FZF_THEN_GREP = function()
+            local builtin = require("telescope.builtin")
+
+            builtin.find_files({
+               attach_mappings = function(prompt_bufnr)
+                  local actions = require("telescope.actions")
+                  local action_state = require("telescope.actions.state")
+                  actions.select_default:replace(function()
+                     local current_picker = action_state.get_current_picker(prompt_bufnr)
+                     local selections = current_picker:get_multi_selection()
+                     -- if no multi-selection, leverage current selection
+                     if vim.tbl_isempty(selections) then
+                        table.insert(selections, action_state.get_selected_entry())
+                     end
+                     local paths = vim.tbl_map(function(e)
+                        return e.path
+                     end, selections)
+                     actions.close(prompt_bufnr)
+                     builtin.live_grep({
+                        search_dirs = paths,
+                     })
+                  end)
+                  -- true: attach default mappings; false: don't attach default mappings
+                  return true
+               end,
+            })
+         end
+      end,
    }
 }
