98cb311d49e475d3e78e246f19318c24f8805da5
aleks <aleksmark98@gmail.com>
2025-06-16 21:52:12 +0200

    navbuddy fullscreen exploration

--- lua/plugin/navbuddy.lua
+++ lua/plugin/navbuddy.lua
@@ -9,5 +9,32 @@ return {
       { '<leader>nb', '<cmd>Navbuddy<CR>' },
    },
    cmd = {'Navbuddy'},
-   opts = { lsp = { auto_attach = true } },
+   opts = {
+      lsp = { auto_attach = true },
+      window = {
+         border = "single",  -- "rounded", "double", "solid", "none"
+         -- or an array with eight chars building up the border in a clockwise fashion
+         -- starting with the top-left corner. eg: { "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" }.
+         size = "100%",       -- Or table format example: { height = "40%", width = "100%"}
+         position = "50%",   -- Or table format example: { row = "100%", col = "0%"}
+         scrolloff = nil,    -- scrolloff value within navbuddy window
+         sections = {
+            left = {
+               size = "20%",
+               border = nil, -- You can set border style for each section individually as well.
+            },
+            mid = {
+               size = "20%",
+               border = nil,
+            },
+            right = {
+               -- No size option for right most section. It fills to
+               -- remaining area.
+               border = nil,
+               preview = "always",  -- Right section can show previews too.
+               -- Options: "leaf", "always" or "never"
+            }
+         },
+      },
+   },
 }
