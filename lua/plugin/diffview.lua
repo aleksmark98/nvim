e6ceb24af7a3b10513dcf7604b2becbafd09550d
aleks <aleksmark98@gmail.com>
2025-06-22 11:19:43 +0200

    diffview: change <c-f>,<c-b> to more intuitive <c-n>,<c-p>

--- lua/plugin/diffview.lua
+++ lua/plugin/diffview.lua
@@ -10,4 +10,27 @@ return {
       vim.cmd("cnoreabbrev dvc DiffviewClose")
       vim.cmd("cnoreabbrev dvh DiffviewFileHistory")
    end,
+   config = function()
+      local actions = require("diffview.actions")
+      require("diffview").setup({
+         keymaps = {
+            view = {
+               { "n", "<c-n>",          actions.select_next_entry,              { desc = "Open the diff for the next file" } },
+               { "n", "<c-p>",          actions.select_prev_entry,              { desc = "Open the diff for the previous file" } },
+            },
+            file_panel = {
+               { "n", "<c-u>",          actions.scroll_view(-0.25),             { desc = "Scroll the view up" } },
+               { "n", "<c-d>",          actions.scroll_view(0.25),              { desc = "Scroll the view down" } },
+               { "n", "<c-n>",          actions.select_next_entry,              { desc = "Open the diff for the next file" } },
+               { "n", "<c-p>",          actions.select_prev_entry,              { desc = "Open the diff for the previous file" } },
+            },
+            file_history_panel = {
+               { "n", "<c-u>",         actions.scroll_view(-0.25),          { desc = "Scroll the view up" } },
+               { "n", "<c-d>",         actions.scroll_view(0.25),           { desc = "Scroll the view down" } },
+               { "n", "<c-n>",         actions.select_next_entry,           { desc = "Open the diff for the next file" } },
+               { "n", "<c-p>",         actions.select_prev_entry,           { desc = "Open the diff for the previous file" } },
+            },
+         },
+      })
+   end,
 }
