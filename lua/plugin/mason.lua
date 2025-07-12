e7861aae3d75e4629158497d07126eb2161230f2
aleks <aleksmark98@gmail.com>
2025-07-12 09:25:09 +0200

    mason: ensure_installed does not work, only ensure_installed for mason-lspconfig, and that applies to lsps only. So AFAIK currently there is no way to ensure_installed formatters

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -1,22 +1,35 @@
 return {
    "mason-org/mason-lspconfig.nvim",
-   opts = {},
+   opts = {
+      ensure_installed = {
+         "clangd",
+         "cmake",
+         "lua_ls",
+         "rust_analyzer",
+         "pyright",
+         "zls",
+      },
+   },
    event = {"VeryLazy"}, -- not sure if this breaks anything
    dependencies = {
       {
          "mason-org/mason.nvim",
          cmd = {"Mason"},
          opts = {
-            ensure_installed = {
-               "clangd",
-               "cmake",
-               "lua_ls",
-               "stylua",
-               "pyright",
-               "yapf",
-               "zls",
-            },
-         }
+            -- this doesn't work anymore :(
+            -- ensure_installed = {
+            --    "clangd",
+            --    "clang-format",
+            --    "cmake",
+            --    "lua_ls",
+            --    "stylua",
+            --    "rustfmt",
+            --    "rust-analyzer",
+            --    "pyright",
+            --    "yapf",
+            --    "zls",
+            -- },
+         },
       },
       "neovim/nvim-lspconfig",
    },
